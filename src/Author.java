9c5d78afc8be63dd5954df95d0c0e2e5064549e9
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-12 12:24:52 -0600

    easy-fix branch

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("all")
 public class Author extends Person {
     private List books;
 
@@ -28,7 +28,6 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
